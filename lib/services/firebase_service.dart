46df04111a44dff7c0aaeeb61fd2c23a1d5865de
iairbaron <84358470+iairbaron@users.noreply.github.com>
2023-07-09 22:56:54 -0300

    Update firebase_service.dart

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,11 +1,8 @@
-// Importamos el paquete cloud_firestore que nos permite interactuar con Cloud Firestore
 
 import "package:cloud_firestore/cloud_firestore.dart";
 
-// Creamos una instancia de Firestore que nos permite acceder a la base de datos de Firestore
 FirebaseFirestore db = FirebaseFirestore.instance;
 
-// Función asincornica para obtener la lista de  que querramos
 Future<List> getColeccion(String coleccion) async {
   List documentos = [];
 
